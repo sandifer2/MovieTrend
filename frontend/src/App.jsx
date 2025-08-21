e3917c6c1c7bce482e3a5126755588edc24c9d79
Zach <zsandifer@icloud.com>
2025-08-20 21:57:38 -0400

    delete and restructure of app.jsx and index.css (to tailwind)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-
-
-const Card = ({title}) =>{
-    // [variable, setVariable] = hook(defaultValue : value);
-  const [hasLiked, setHasLiked] = useState(false);
-  
-  return(
-    <div className="card">
-      <h2>{title}</h2>
-      <button onClick={() => setHasLiked(!hasLiked)}>
-      {hasLiked ? 'Liked' : 'Like'}
-      </button>
-    </div>
-  )
-}
-
-
-const App = () =>{
-
-
-
+const App = () => {
   return (
-    <div className="card-container">
-      <Card title="Star Wars" rating={5} isCool={true} />
-      <Card title="Avatar"/>
-      <Card title="Kung-Fu Panda"/>
-    </div>
+    <main>
+      <div className='pattern'/>
+      <div className='wrapper'>
+        <header>
+          <img src='./hero.png' alt='hero banner'/>
+          <h1>
+            Find <span className='text-gradient'>Movies</span> You'll enjoy without the hassle
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  useEffect(() => {
+    console.log('hello');
+  }, [hasLiked]); //deps - dependency array, only if changed effect is called
+
+  //useEffect(()=> {}, []) runs only on first app/mount of component
+  */
