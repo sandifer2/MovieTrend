d2960f4256737d53884f704a84fadf45196fc5b9
Zach <zsandifer@icloud.com>
2025-08-23 18:40:59 -0400

    implemented debouncing on search for performance optimization

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { useDebounce } from 'react-use'
+
 import Search from './components/Search'
+import MovieCard from './components/MovieCard'
+import Spinner from './components/Spinner'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
@@ -18,15 +22,18 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState('');
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
 
-  const fetchMovies = async () => {
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 1000, [searchTerm]);
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
     try{
 
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`:`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS); 
 
       if(!response.ok){
@@ -52,8 +59,8 @@ const App = () => {
   }
   
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm]);
   return (
     <main>
       <div className='pattern'/>
@@ -69,13 +76,13 @@ const App = () => {
           <h2 className='mt-[40px]'>All Movies</h2>
 
         {isLoading ? (
-          <Spinner/>
+          <Spinner />
         ) : errorMessage ? (
           <p className='text-red-500'>{errorMessage}</p>
         ) : (
           <ul>
             {movieList.map((movie) => (
-              <p key={movie.id} className='text-white'>{movie.title}</p>
+              <MovieCard key={movie.id} movie={movie}/>
             ))}
           </ul>
         )}
