5a6871d5c5314d26e6f2a04a90ff3f29b30e5521
Zach <zsandifer@icloud.com>
2025-08-23 20:47:12 -0400

    added appwrite backend initial file

---
+++ frontend/src/appwrite.js
@@ -0,0 +1,3 @@
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+const ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
