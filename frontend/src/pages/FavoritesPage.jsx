9ebdbe0b7043808db6c9a70b17532ed4b5f3186a
Zach <zsandifer@icloud.com>
2025-09-09 22:58:55 -0400

    placeholder for future favorites page

---
+++ frontend/src/pages/FavoritesPage.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState} from 'react';
+
+
+const FavoritesPage = () => {
+    return (
+        <div className='favorites-page'>
+            <h1>FavoritesPage</h1>
+        </div>
+    )
+}
+
+export default FavoritesPage;
