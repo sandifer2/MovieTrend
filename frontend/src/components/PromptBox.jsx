0a33b75d7b04348fbcadad4de7120d5e3bffcca6
Zach <zsandifer@icloud.com>
2025-09-30 21:57:13 -0400

    Added functionality to move to ai page, formatted page and created boilerplate for the prompt box component

---
+++ frontend/src/components/PromptBox.jsx
@@ -0,0 +1,23 @@
+import { div } from 'framer-motion/client';
+import React, { useEffect, useState } from 'react';
+
+const PromptBox = () =>{
+
+
+
+
+
+return (
+    <div className='movie-card'>
+        <h1> Hey</h1>
+    </div>
+)
+
+
+}
+
+
+
+
+
+export default PromptBox
--- frontend/src/pages/RecommendationPage.jsx
+++ frontend/src/pages/RecommendationPage.jsx
@@ -1,9 +1,26 @@
 import React, { useEffect, useState} from 'react';
+import Spinner from '../components/Spinner';
+import MovieCard from '../components/MovieCard';
+
 
 
 
 const RecommendationPage = () => {
     return (
-        <div className='pattern'> 
-        </div>) 
+        <div className='pattern'>
+            <div className='wrapper'>
+                <header>
+                    <h1>Your <span className='text-gradient'>AI</span> Movie Finding Agent</h1>
+                </header>
+                <section className='movie-card'>
+                    <h6 className='text-white'>this is not gonna work</h6>
+                </section>
+
+
+
+
+            </div>
+        </div>)
 }
+
+export default RecommendationPage;
