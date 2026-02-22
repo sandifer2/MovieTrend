2ba0ff3a9aabd25141d75c116cd78fb5f2d266c9
Zach <zsandifer@icloud.com>
2026-02-22 16:31:39 -0500

    changed to add protection on routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,18 +5,28 @@ import Navigation from './components/Navigation';
 import FavoritesPage from './pages/FavoritesPage';
 import RecommendationPage from './pages/RecommendationPage';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Gatekeeper from './services/Gatekeeper';
+import { AuthProvider } from './services/AuthContext';
 
 
 const App = () => {
   return (
     <Router>
-      <Navigation />
+      <AuthProvider>
+      <Navigation/>
       <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/favorites' element={<FavoritesPage />} />
-        <Route path='/recommendations' element={<RecommendationPage/>}/>
+
+        <Route element={ <Gatekeeper/>}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/favorites' element={<FavoritesPage />} />
+          <Route path='/recommendations' element={<RecommendationPage/>}/>
+        </Route>
+
         <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/register' element={<RegisterPage/>}/>
       </Routes>
+      </AuthProvider>
     </Router>
   );
 }
