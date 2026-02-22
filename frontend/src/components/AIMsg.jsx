4e003566cb7c37b362fac00b3483818a26a60e5c
Zach <zsandifer@icloud.com>
2026-02-22 16:31:54 -0500

    warning change

--- frontend/src/components/AIMsg.jsx
+++ frontend/src/components/AIMsg.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const AIMsg = ({ msg }) => {
     return(
 
-        <div className="max-w-[75%] w-fit bg-gradient-to-br from-[#D6C7FF]/15 to-[#AB8BFF]/25 backdrop-blur-md p-4 rounded-2xl rounded-tl-sm border border-[#cecefb]/15 shadow-lg">
-            <p className="text-white text-sm break-words leading-relaxed">{msg}</p>
+        <div className="max-w-[75%] w-fit bg-linear-to-br from-[#D6C7FF]/15 to-[#AB8BFF]/25 backdrop-blur-md p-4 rounded-2xl rounded-tl-sm border border-light-100/15 shadow-lg">
+            <p className="text-white text-sm wrap-break-word leading-relaxed">{msg}</p>
         </div>
     )
 }
